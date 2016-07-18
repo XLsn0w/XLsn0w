4120224667a90a1c751e061259ff97ee1349cfc2
XLsn0w <xlsn0w@qq.com>
2016-07-18 15:51:11 +0800

    add

--- XLsn0wFramework/XLNSObject/XLMethods.m
+++ XLsn0wFramework/XLNSObject/XLMethods.m
@@ -202,17 +202,16 @@
     NSString *documentsDirectoryPath = [paths objectAtIndex:0];
     return documentsDirectoryPath;
 }
-+ (void)showTipsWithHUD:(NSString *)labelText showTime:(CGFloat)time
-{
-    MBProgressHUD *hud = [[MBProgressHUD alloc] initWithWindow:[[[UIApplication sharedApplication] delegate] window]] ;
++ (void)showTipsWithHUD:(NSString *)labelText showTime:(CGFloat)time {
+    MBProgressHUD *hud = [[MBProgressHUD alloc] initWithView:[[[UIApplication sharedApplication] delegate] window]];
     hud.mode = MBProgressHUDModeText;
-    hud.detailsLabelText = labelText;
-    hud.detailsLabelFont = [UIFont systemFontOfSize:15.0];
+    hud.detailsLabel.text = labelText;
+    hud.detailsLabel.font = [UIFont systemFontOfSize:15.0];
     hud.removeFromSuperViewOnHide = YES;
-    [hud show:YES];
+    [hud showAnimated:YES];
     [[[[UIApplication sharedApplication] delegate] window] addSubview:hud];
     
-    [hud hide:YES afterDelay:time];
+    [hud hideAnimated:YES afterDelay:time];
 }
 
 + (void)showTipsWithHUD:(NSString *)labelText
@@ -229,33 +228,26 @@
 {
     MBProgressHUD *hud = [[MBProgressHUD alloc] initWithView:uiview] ;
     hud.mode = MBProgressHUDModeText;
-    hud.detailsLabelText = labelText;
-    hud.detailsLabelFont = [UIFont systemFontOfSize:15.0];
+    hud.detailsLabel.text = labelText;
+    hud.detailsLabel.font = [UIFont systemFontOfSize:15.0];
     hud.removeFromSuperViewOnHide = YES;
-    [hud show:YES];
+    [hud showAnimated:YES];
     [uiview addSubview:hud];
     
-    [hud hide:YES afterDelay:time];
+    [hud hideAnimated:YES afterDelay:time];
 }
 + (void)showProgessInView:(UIView *)view withExtBlock:(void (^)())exBlock withComBlock:(void (^)())comBlock
 {
     MBProgressHUD *hud = [[MBProgressHUD alloc]initWithView:view];
-    hud.color = [UIColor colorWithWhite:0.8 alpha:0.6];
+    hud.bezelView.color = [UIColor colorWithWhite:0.8 alpha:0.6];
     //    hud.dimBackground = NO;
     [view addSubview:hud];
-    hud.labelText = @"正在加载...";
+    hud.label.text = @"正在加载...";
     if (exBlock) {
-        [hud showAnimated:YES whileExecutingBlock:exBlock completionBlock:^{
-            if (comBlock) {
-                comBlock();
-            }
-            [hud removeFromSuperview];
-        }];
-        
-    }else
-        [hud showAnimated:YES whileExecutingBlock:exBlock completionBlock:^{
-            [hud removeFromSuperview];
-        }];
+        [hud removeFromSuperview];
+    }else {
+      [hud removeFromSuperview];
+    }
 }
 
 + (void) showHudMessage:(NSString*) msg hideAfterDelay:(NSInteger) sec uiview:(UIView *)uiview
@@ -263,18 +255,17 @@
     
     MBProgressHUD* hud2 = [MBProgressHUD showHUDAddedTo:uiview animated:YES];
     hud2.mode = MBProgressHUDModeText;
-    hud2.labelText = msg;
+    hud2.label.text = msg;
     hud2.margin = 12.0f;
-    hud2.yOffset = 20.0f;
+    [hud2 setOffset:(CGPointMake(0, 20))];
     hud2.removeFromSuperViewOnHide = YES;
-    [hud2 hide:YES afterDelay:sec];
+    [hud2 hideAnimated:YES afterDelay:sec];
 }
 
 
 
 
-+ (void)showNotReachabileTips
-{
++ (void)showNotReachabileTips {
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil
                                                         message:@"与服务端连接已断开,请检查您的网络连接是否正常."
                                                        delegate:self
