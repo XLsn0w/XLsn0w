29421db0b0dfb539feba5cfd16a94dfc5312a292
XLsn0w <xlsn0w@qq.com>
2016-08-17 14:55:38 +0800

    v4.1.7

--- XLsn0wFramework/XLDepend/XLDepend.h
+++ XLsn0wFramework/XLDepend/XLDepend.h
@@ -61,9 +61,9 @@
 
 //DEBUG  模式下打印日志,当前行 并弹出一个警告
 #ifdef DEBUG
-#   define ULog(fmt, ...)  { UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
+#   define XLLog(fmt, ...)  { UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]; [alert show]; }
 #else
-#   define ULog(...)
+#   define XLLog(...)
 #endif
 
 
