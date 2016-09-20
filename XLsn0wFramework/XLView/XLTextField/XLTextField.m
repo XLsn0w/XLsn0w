5ddffb98c2f6aff71a37c4fe7c1441f5a12c4cef
XLsn0w <xlsn0w@qq.com>
2016-09-20 17:04:22 +0800

    v4.6.3

--- XLsn0wFramework/XLView/XLTextField/XLTextField.m
+++ XLsn0wFramework/XLView/XLTextField/XLTextField.m
@@ -5,20 +5,20 @@
 
 //控制placeHolder的位置，左右缩20
 - (CGRect)placeholderRectForBounds:(CGRect)bounds {
-    CGRect inset = CGRectMake(bounds.origin.x+20, bounds.origin.y, bounds.size.width -10, bounds.size.height);
+    CGRect inset = CGRectMake(bounds.origin.x + 40, bounds.origin.y, bounds.size.width -10, bounds.size.height);
     return inset;
 }
 
 //控制显示文本的位置
 -(CGRect)textRectForBounds:(CGRect)bounds {
-    CGRect inset = CGRectMake(bounds.origin.x+20, bounds.origin.y, bounds.size.width -10, bounds.size.height);
+    CGRect inset = CGRectMake(bounds.origin.x + 40, bounds.origin.y, bounds.size.width -10, bounds.size.height);
     return inset;
     
 }
 
 //控制编辑文本的位置
 -(CGRect)editingRectForBounds:(CGRect)bounds {
-    CGRect inset = CGRectMake(bounds.origin.x +20, bounds.origin.y, bounds.size.width -10, bounds.size.height);
+    CGRect inset = CGRectMake(bounds.origin.x + 40, bounds.origin.y, bounds.size.width -10, bounds.size.height);
     return inset;
 }
 
