b6f05fddc7685cef367a7ecf0b3790fe93e6a28a
XLsn0w <xlsn0w@qq.com>
2016-10-11 16:25:33 +0800

    update v 5.0.1

--- XLsn0wFramework/XLCategory/UIColor/UIColor+JKHEX.m
+++ XLsn0wFramework/XLCategory/UIColor/UIColor+JKHEX.m
@@ -1,12 +1,7 @@
-//
-//  UIColor+HEX.m
-//  JKCategories (https://github.com/shaojiankui/JKCategories)
-//
-//  Created by Jakey on 14/12/15.
-//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
-//
+
 
 #import "UIColor+JKHEX.h"
+
 CGFloat jk_colorComponentFrom(NSString *string, NSUInteger start, NSUInteger length) {
     NSString *substring = [string substringWithRange:NSMakeRange(start, length)];
     NSString *fullHex = length == 2 ? substring : [NSString stringWithFormat: @"%@%@", substring, substring];
