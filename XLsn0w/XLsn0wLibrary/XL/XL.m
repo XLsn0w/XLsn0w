42bbe46e62feaf06353af270ffc1a839df1e6f8e
XLsn0w <xlsn0w@qq.com>
2016-11-02 10:24:27 +0800

    update v 5.1.6

--- XLsn0w/XLsn0wLibrary/XL/XL.m
+++ XLsn0w/XLsn0wLibrary/XL/XL.m
@@ -209,10 +209,56 @@
 
 //压缩图片
 + (UIImage *)xl_getCompressedImageWithNewSize:(CGSize)newSize currentImage:(UIImage *)currentImage {
-    UIGraphicsBeginImageContext(newSize);
-    [currentImage drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-    UIImage *compressedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIImage *sourceImage = currentImage;
+    UIImage *compressedImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = newSize.width;
+    CGFloat targetHeight = newSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
+    
+    if (CGSizeEqualToSize(imageSize, newSize) == NO) {
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if (widthFactor > heightFactor)
+            scaleFactor = widthFactor; // scale to fit height
+        else
+            scaleFactor = heightFactor; // scale to fit width
+        scaledWidth= width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        // center the image
+        if (widthFactor > heightFactor)
+        {
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }
+        else if (widthFactor < heightFactor)
+        {
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    UIGraphicsBeginImageContext(newSize); // this will crop
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width= scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    compressedImage = UIGraphicsGetImageFromCurrentImageContext();
+    if(compressedImage == nil)
+        NSLog(@"could not scale image");
+    
+    //pop the context to get back to the default
     UIGraphicsEndImageContext();
+    
     return compressedImage;
 }
 
